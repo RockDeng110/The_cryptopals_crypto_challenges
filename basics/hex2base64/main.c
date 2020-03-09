71d96246a2236e0e78cce0e988c783d7de628cca
Rock Deng <dengyongpeng110@outlook.com>
2020-03-09 10:13:47 +0800

    complete

--- basics/hex2base64/main.c
+++ basics/hex2base64/main.c
@@ -24,7 +24,8 @@ int main(int argc, char ** argv){
     int num_o, num_t;
 
     printf("Original hex string:\n%s\n", hex);
-    printf("Target base64 string:\n%s\n\n", base64);
+    printf("Target base64 string:\n%s\n", base64);
+    printf("Base64 table:\n%s\n\n", base64_table);
     num_o = strlen(hex);
     num_t = strlen(base64);
     printf("Number of characters in original: %d\n", num_o);
@@ -32,8 +33,14 @@ int main(int argc, char ** argv){
     //char output[num_t+1] = "";
     char * output;
     output = hex2base64((char *)hex, num_o);
-    printf("Converted string is %s", output);
-
+    printf("Converted string is: \n%s\n", output);
+    int cmp_result;
+    cmp_result = strcmp(output, base64);
+    if (cmp_result == 0){
+        printf("Converted string == Target string\n");
+    } else{
+        printf("Converted string != Target string\n");
+    }
 
     free(output);
     return EXIT_SUCCESS;
@@ -70,36 +77,36 @@ char* hex2base64(char *hex, int char_num){
     } else {
 
     }
-    printf("hex2base64 hex:\n");
-    printf("%s\n", hex);
+//    printf("hex2base64 hex:\n");
+//    printf("%s\n", hex);
     int num_base64 = char_num / 3 * 2;
     char *out = malloc(sizeof(char) * (num_base64 + 1));
+    char *return_value = out;
     *(out + num_base64) = '\0';
     unsigned int base_char = 0;
     unsigned int num = 0;
     char low_letter, high_letter;
-    printf(" char_num=%d\n", char_num);
+//    printf(" char_num=%d\n", char_num);
     for (int i=0; i<char_num/3; i++){
-        printf(" i = %d; letters: 0x0%c%c%c\n", i, *hex, *(hex+1), *(hex+2));
+        printf(" i = %2d; letters: 0x0%c%c%c\n", i, *hex, *(hex+1), *(hex+2));
         unsigned int base = 0;
         num = 0;
         for (int j=0; j<3; j++){
             num <<= 4;
             num |= char2num(*(hex++));
-            printf("num=0x%02x\n", num);
+//            printf("num=0x%02x\n", num);
         }
-        printf(" binary form: ");
-        printf_binary(num);
-        printf("\n");
-        printf(" base64 indexes: %2d %2d\n", num >> 6, num & 0x3f);
+//        printf(" binary form(reverse order): ");
+//        printf_binary(num);
+//        printf("\n");
+//        printf(" base64 indexes: %2d %2d\n", num >> 6, num & 0x3f);
         low_letter = base64_table[num & 0x3f];
         high_letter = base64_table[num >> 6];
         *(out++) = high_letter;
         *(out++) = low_letter;
     }
-    printf("exit hex2base64\n");
 
-    return out;
+    return return_value;
 }
 
 
