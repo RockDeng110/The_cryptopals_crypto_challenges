a0d461d91f6306fed2d4bf3d36d528061c2fe439
Rock Deng <dengyongpeng110@outlook.com>
2020-03-12 17:45:37 +0800

    edit all function

--- basics/single-byte-xor/main.c
+++ basics/single-byte-xor/main.c
@@ -3,7 +3,7 @@
 * @Breif 
 * @author Rock Deng
 * @version 1.0
-* @date 2020-03-09
+* @date 2020-03-12
 */
 
 
@@ -12,20 +12,113 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <math.h>
 
 
 
-unsigned int char2num(char c);
+const char *hex_string = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+float english_freq[26] = {
+0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015,  // A-G
+0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406, 0.06749,  // H-N
+0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758,  // O-U
+0.00978, 0.02360, 0.00150, 0.01974, 0.00074                     // V-Z
+};
+float rate_array [256][26] = {0};
+
+
+unsigned int hexchar2hexnum(char c);
 char * string2array(char * c);
 char * xor_buffer(char * buffer1, char *buffer2, int buffer_len);
 char hexnum2char(char num);
 char * hexarray2string(char * array, int array_len);
+int hexstring2hexarray(char *hexstring, char *hexarray);
 
 
 int main(int args, char ** argv){
 
+    ///< convert hex_string to hex array
+    char * hex_array, decrypt_array;
+    int hex_array_len;
+    hex_array_len = hexarray2string(hex_string, hex_array);
+    decrypt_array = malloc(sizeof(*decrypt_array) * hex_array_len);
+    float rate = 0;
+    int count = 0;
+    
+    ///< try key from 0x00 to 0xff
+    for (int i=0; i<0xff; i++){
+        ///< decrypt message
+        for (int j=0; j<hex_array_len; j++){
+            *(decrypt_array + j) = *(hex_array + j) ^ i;
+        }
+        ///< calculate rate of each letter in the message
+        for (int k='a'; k<='z'; k++){
+            for (int m=0; m<hex_array_len; m++){
+                if (k == *(decrypt_array + m)){
+                    count++;
+                }
+            }
+            count = 0;
+            rate = count / (float)hex_array_len;
+            rate_array[i][k-'a'] = rate;
+        }
+    }
+    ///< find the high score corresponding the character frequency in English plaintext.
+    
+    float score_array[256] = {0};
+    for (int i=0; i<256; i++){
+        for (int j=0; j<26; j++){
+            abs_value = fabs(rate_array[i][j], english_freq[j]);
+            kk = abs_value * abs_value / english_freq[j];
+            score_array[i] += kk;
+
+        }
+    }
+
+    ///< find the least score number
+    float least_num = score_array[0];
+    int key = 0;
+    for (int i=0; i<256; i++){
+        if (score_array[i] < least_num){
+            least_num = score_array[i];
+            key = i;
+        }
+    }
+
+
+    printf("the key is 0x%x", key);
+    free(hex_array);
+    free(decrypt_array);
 
     return EXIT_SUCCESS;
+}
+
+
+int hexstring2hexarray(char *hexstring, char *hexarray){
+
+    assert(hexstring != NULL);
+    int str_len = strlen(hexstring);
+    if (str_len % 2 == 1){
+        array_len = str_len / 2 + 1;
+    } else {
+        array_len = str_len / 2;
+    }
+    hexarray = malloc(sizeof(*hexarray) * array_len);
+
+    int j = 0;
+    char result = 0;
+    for (int i=0; i<str_len;){
+        if (*(hexarray + i + 1) == '\0'){
+            result = hexchar2hexnum(*(hexstring + i));
+            break;
+        } else {
+            result = hexchar2hexnum(*(hexstring + i)) << 4 + hexchar2hexnum(*(hexarra + i + 1));
+
+        }
+        *(hexarray + j++) = result;
+        i += 2;
+    }
+
+    return array_len;
 }
 
 char * xor_buffer(char * buffer1, char *buffer2, int buffer_len){
@@ -86,7 +179,7 @@ char * string2array(char * c){
     }
     char * array = malloc(sizeof(*array) * str_len);
     for (int i=0; i<str_len; i++){
-        *(array + i) = (char)char2num(*(c + i));
+        *(array + i) = (char)hexchar2hexnum(*(c + i));
     }
 
     printf("array:  ");
@@ -100,7 +193,7 @@ char * string2array(char * c){
 
 
 
-unsigned int char2num(char c){
+unsigned int hexchar2hexnum(char c){
     assert((c <= '9' && c >= 0) || (c <= 'f' && c >= 'a'));
     unsigned int n;
     if (c > '9'){
