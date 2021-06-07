ca9e95ea835d02d7bf74163dbf87a52772f15657
RockDeng <dengyongpeng110@outlook.com>
2021-06-07 22:33:21 +0800

    add python script for hex2base64

---
+++ basics/hex2base64/hex2base64.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import codecs
+
+hex_str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+hex = codecs.decode(hex_str, 'hex') # decode it from bytes to hex string 
+print(hex)
+
+b64 = codecs.encode(hex, 'base64').decode() # encode it from string to base64 bytes and then decode it to string
+
+print(b64)
